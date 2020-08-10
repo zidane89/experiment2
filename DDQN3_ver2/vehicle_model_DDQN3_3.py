c8b52848d2d6f3c3b7688a2ba887925b9e171363
zidane89 <zidane208@naver.com>
2020-08-10 17:32:12 +0900

    Add files via upload

--- DDQN3_ver2/vehicle_model_DDQN3_3.py
+++ DDQN3_ver2/vehicle_model_DDQN3_3.py
@@ -17,7 +17,7 @@ class Environment:
         self.version = 2
 
         self.vehicle_comp = {
-            "m_veh": 900,
+            "m_veh": 1900,
             "cd": 0.456,
             "fa": 2.4,
             "rr": 0.009,
@@ -28,8 +28,8 @@ class Environment:
             "g": 9.8,
         }
         self.stack_comp = {
-            "cell_number": 200,
-            "effective_area_cell": 200.0,
+            "cell_number": 400,
+            "effective_area_cell": 400.0,
             "max_current_density": 1.0,
             "idling_current_density": 0.01,
             "Faraday_constant": 96485,
@@ -172,7 +172,7 @@ class Environment:
 
             if np.isnan(self.SOC):
                 done = True
-                reward = -10000
+                reward = -1000
                 print("SOC is nan...")
 
         return state, reward, done
